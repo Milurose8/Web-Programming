a6936bc65ae7cafe73ff8a07d580300882853785
Milurose8 <74668175+Milurose8@users.noreply.github.com>
2021-03-18 15:34:50 +0530

    Add files via upload

---
+++ CYCLE 5/C5.8.php
@@ -0,0 +1,27 @@
+<html>
+<body
+<?php
+$servername = "localhost";
+$username = "20mca036";
+$password = "2605";
+$dbname = "20mca036";
+
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+  die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "UPDATE STUDENT SET mark='90' WHERE id=2";
+
+if (mysqli_query($conn, $sql)) {
+  echo "Record updated successfully !";
+} else {
+  echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
+?>
+</body>
+</html>
